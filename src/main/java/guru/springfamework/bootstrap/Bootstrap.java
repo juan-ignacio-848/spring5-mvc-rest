c28349c7eb13bf2348985b75b5d9e25046b14abe
nmkip <juan.ignacio848@gmail.com>
2018-08-18 15:04:07 -0300

    Get customers

--- src/main/java/guru/springfamework/bootstrap/Bootstrap.java
+++ src/main/java/guru/springfamework/bootstrap/Bootstrap.java
@@ -1,21 +1,45 @@
 package guru.springfamework.bootstrap;
 
 import guru.springfamework.domain.Category;
+import guru.springfamework.domain.Customer;
 import guru.springfamework.repositories.CategoryRepository;
+import guru.springfamework.repositories.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Bootstrap implements CommandLineRunner {
 
-    private CategoryRepository categoryRepository;
+    private final CategoryRepository categoryRepository;
+    private final CustomerRepository customerRepository;
 
-    public Bootstrap(CategoryRepository categoryRepository) {
+    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
         this.categoryRepository = categoryRepository;
+        this.customerRepository = customerRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        loadCategories();
+        loadCustomers();
+
+    }
+
+    private void loadCustomers() {
+        Customer susanTanner = new Customer();
+        susanTanner.setFirstname("Susan");
+        susanTanner.setLastname("Tanner");
+
+        Customer titoValentino = new Customer();
+        titoValentino.setFirstname("Tito");
+        titoValentino.setLastname("Valentino");
+
+        customerRepository.save(susanTanner);
+        customerRepository.save(titoValentino);
+    }
+
+    private void loadCategories() {
         Category fruits = new Category();
         fruits.setName("Fruits");
 
@@ -37,6 +61,6 @@ public class Bootstrap implements CommandLineRunner {
         categoryRepository.save(exotic);
         categoryRepository.save(nuts);
 
-        System.out.println("Data loaded = " + categoryRepository.count());
+        System.out.println("Category Data loaded = " + categoryRepository.count());
     }
 }
---
+++ src/test/java/guru/springfamework/controllers/v1/CustomerControllerTest.java
@@ -0,0 +1,76 @@
+package guru.springfamework.controllers.v1;
+
+import guru.springfamework.api.v1.model.CustomerDTO;
+import guru.springfamework.services.CustomerService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class CustomerControllerTest {
+
+    private static final String FIRSTNAME = "Foo";
+    private static final String LASTNAME = "Bar";
+    private static final String CUSTOMER_URL = "/shop/customer/1";
+
+    MockMvc mockMvc;
+
+    @Mock
+    CustomerService customerService;
+
+    @InjectMocks
+    CustomerController customerController;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(customerController).build();
+    }
+
+    @Test
+    public void getAllCustomers() throws Exception {
+
+        List<CustomerDTO> customers = Arrays.asList(new CustomerDTO(), new CustomerDTO(), new CustomerDTO());
+
+        when(customerService.getAllCustomers()).thenReturn(customers);
+
+        mockMvc.perform(get("/api/v1/customers")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.customers", hasSize(3)));
+    }
+
+    @Test
+    public void getCustomerById() throws Exception {
+
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setFirstname(FIRSTNAME);
+        customerDTO.setLastname(LASTNAME);
+        customerDTO.setCustomerUrl(CUSTOMER_URL);
+
+        when(customerService.getCustomerById(anyLong())).thenReturn(customerDTO);
+
+        mockMvc.perform(get("/api/v1/customers/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstname", equalTo(FIRSTNAME)))
+                .andExpect(jsonPath("$.lastname", equalTo(LASTNAME)))
+                .andExpect(jsonPath("$.customerUrl", equalTo(CUSTOMER_URL)));
+    }
+}
